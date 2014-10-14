ce133eb3f04a60dee7e984e078f2a0319ac6a90e
davidkeltgen <dkeltgen45@gmail.com>
2014-10-14 02:18:40 -0600

    first commit for general_assignment_7_pt1_qs_nonthreaded_java

---
+++ general_assignment_pt1_qs_nonthreaded_java/src/general_assignment_pt1_qs_nonthreaded_java/General_assignment_pt1_qs_nonthreaded_java.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package general_assignment_pt1_qs_nonthreaded_java;
+
+/**
+ *
+ * @author david
+ */
+public class General_assignment_pt1_qs_nonthreaded_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
