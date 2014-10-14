cd7194d7b49e080705ceef63fa955228ff3b5f02
davidkeltgen <dkeltgen45@gmail.com>
2014-10-14 01:08:25 -0600

    first commit for general assignment 3

---
+++ gen_assign_3/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: david
+ *
+ * Created on October 14, 2014, 1:08 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
